856f3dd07052ac8679faa1517a5d0235fbf5e376
Jan Nemec <jnemec@acrobits.cz>
2015-02-10 15:46:18 +0100

    layout

--- src/cz/honza/bluetooth/BluetoothActivity.java
+++ src/cz/honza/bluetooth/BluetoothActivity.java
@@ -1,7 +1,14 @@
 package cz.honza.bluetooth;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class BluetoothActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.bluetooth);
+	}
 
 }
